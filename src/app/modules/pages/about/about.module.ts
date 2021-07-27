3c52f9f313a678d0d985815e9d63ba09488b79de
SaDWicKED <56205681+SaDWicKED@users.noreply.github.com>
2021-07-27 19:13:43 -0400

    Implemented About Page

--- src/app/modules/pages/about/about.module.ts
+++ src/app/modules/pages/about/about.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutPage } from './about.page';
+import { PolicyComponent } from './policy/policy.component';
+import {MaterialModule} from "../../material/material.module";
 
 
 @NgModule({
   declarations: [
-    AboutPage
+    AboutPage,
+    PolicyComponent
   ],
   imports: [
     CommonModule,
-    AboutRoutingModule
+    AboutRoutingModule,
+    MaterialModule
   ]
 })
 export class AboutModule { }
--- src/app/modules/pages/about/about.page.ts
+++ src/app/modules/pages/about/about.page.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import {SidenavService} from "../../shared/services/sidenav.service";
+import {HeaderService} from "../../ui/header/header.service";
+import {PolicyComponent} from "./policy/policy.component";
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.page.html',
   styleUrls: ['./about.page.scss']
 })
-export class AboutPage implements OnInit {
+export class AboutPage implements OnDestroy {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private dialog: MatDialog,
+              private sidenavService: SidenavService,
+              private headerService: HeaderService) {
+    headerService.setHeight(65);
   }
 
+  onPolicyClick(): void {
+    this.dialog.open(PolicyComponent, {
+      height: '80%',
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.headerService.setHeight(300);
+  }
 }
