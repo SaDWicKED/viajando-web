aa87eadc8d70f607460cbde3f6ebee25e15df3b7
SaDWicKED <56205681+SaDWicKED@users.noreply.github.com>
2021-08-17 13:29:47 -0400

    Implemented availability.service

--- src/app/modules/shared/services/availability.service.ts
+++ src/app/modules/shared/services/availability.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import {DispService} from "../../api/availability/services/disp.service";
+import {map} from "rxjs/operators";
+import {Observable} from "rxjs";
+import {Availability} from "../../api/availability/models/availability";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AvailabilityService {
 
-  constructor() { }
+  constructor( private dispService: DispService) { }
+
+  getAvailability(
+    travelType: string,
+    travelMode: string,
+    origin: string,
+    destination: string,
+    depDate: string,
+    cbDate: string): Observable<Availability[]> {
+
+    let availabilities = this.dispService.availabilities({
+      source: origin,
+      target: destination,
+      startDate: depDate,
+      endDate: cbDate
+    });
+
+    if (cbDate === '3000-09-02') {
+      availabilities = availabilities.pipe(
+        map(rawdata =>
+          rawdata.filter(item => {
+            return item.travelType === 'Ida';
+          })
+        )
+      );
+    }
+
+    availabilities = availabilities.pipe(
+      map(rawdata => rawdata.filter(item => item.available!.toString() === 'true'))
+    );
+
+    return availabilities;
+  }
 }
