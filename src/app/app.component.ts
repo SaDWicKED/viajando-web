83e7e6d7b039acbd75eb21957f42ed24ea2b1cdc
SaDWicKED <56205681+SaDWicKED@users.noreply.github.com>
2021-08-18 19:32:49 -0400

    Implemented proper timer behaviour when app goes to background. Now user is logged out after 15 minutes of inactivity time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
+import {Subject} from "rxjs";
+import {Router} from "@angular/router";
+import {TimerService} from "./modules/shared/services/timer.service";
+import {AuthService} from "./modules/shared/services/auth.service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'viajando-web';
+  title = 'viajando';
+
+  private userActivity: any;
+  private userInactive: Subject<any> = new Subject();
+
+  constructor(private router: Router,
+              private timerService: TimerService,
+              private authService: AuthService
+  ) {
+    this.setTimeout();
+    this.userInactive.subscribe(() => {
+      this.authService.logout();
+    });
+  }
+
+  private setTimeout(): void {
+    this.userActivity = setTimeout(() => this.userInactive.next(undefined), 900000);
+  }
+
+  @HostListener('window:mousemove') refreshUserState() {
+    clearTimeout(this.userActivity);
+    this.setTimeout();
+  }
+
+  @HostListener('window:blur') onPause() {
+    sessionStorage.setItem('timeLeft', this.timerService.getTimeLeft().toString());
+    sessionStorage.setItem('timeWhenPaused', Date.now().toString());
+  }
+
+  @HostListener('window:focus') onResume() {
+    if (this.timerService.isTimerRunning()) {
+      const timeLeft = parseInt(sessionStorage.getItem('timeLeft')!, 10) ;
+      const timeWhenPaused = parseInt(sessionStorage.getItem('timeWhenPaused')!, 10);
+      const currentTime = Date.now();
+      const timeLapsed = currentTime - timeWhenPaused;
+
+      if (timeLapsed >= timeLeft * 1000) {
+        location.reload();
+      } else {
+        const remainingTime = timeLeft - timeLapsed / 1000;
+        if (remainingTime >= 0) {
+          this.timerService.setTimeLeft(Math.round(remainingTime));
+        } else {
+          this.timerService.setTimeLeft(0);
+        }
+      }
+    }
+  }
+
 }
