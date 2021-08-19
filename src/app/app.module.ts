bf2f0894b2cc995bb4fc223b533caf2032b7a888
SaDWicKED <56205681+SaDWicKED@users.noreply.github.com>
2021-08-19 12:47:06 -0400

    Prepared app for lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutModule } from './modules/pages/about/about.module';
-import { ActivateAccountModule } from './modules/pages/activate-account/activate-account.module';
-import { AgenciesModule } from './modules/pages/agencies/agencies.module';
-import { BookingModule } from './modules/pages/booking/booking.module';
-import { AvailabilityModule } from './modules/pages/availability/availability.module';
-import { AvailabilityResultModule } from './modules/pages/availability-result/availability-result.module';
-import { FaqsModule } from './modules/pages/faqs/faqs.module';
-import { LoginModule } from './modules/pages/login/login.module';
-import { NotFoundModule } from './modules/pages/not-found/not-found.module';
-import { RegisterModule } from './modules/pages/register/register.module';
-import { ResetPasswordModule } from './modules/pages/reset-password/reset-password.module';
-import { TravelHistoryModule } from './modules/pages/travel-history/travel-history.module';
-import { TravelersModule } from './modules/pages/travelers/travelers.module';
-import { UserProfileModule } from './modules/pages/user-profile/user-profile.module';
 import { HttpClientModule } from "@angular/common/http";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { httpInterceptorProviders } from "./modules/core/interceptors";
@@ -55,20 +41,6 @@ import {QRCodeModule} from "angularx-qrcode";
     HttpClientModule,
     BrowserModule,
     AppRoutingModule,
-    AboutModule,
-    ActivateAccountModule,
-    AgenciesModule,
-    BookingModule,
-    AvailabilityModule,
-    AvailabilityResultModule,
-    FaqsModule,
-    LoginModule,
-    NotFoundModule,
-    RegisterModule,
-    ResetPasswordModule,
-    TravelHistoryModule,
-    TravelersModule,
-    UserProfileModule,
     BrowserAnimationsModule,
     MaterialModule,
     MatPasswordStrengthModule,
