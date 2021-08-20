454315ca12a08407fdb1bdd11ebc9f614fe87c3b
SaDWicKED <56205681+SaDWicKED@users.noreply.github.com>
2021-08-20 16:37:37 -0400

    Implemented travel-history page

--- src/app/modules/pages/travel-history/travel-history.module.ts
+++ src/app/modules/pages/travel-history/travel-history.module.ts
@@ -3,15 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { TravelHistoryRoutingModule } from './travel-history-routing.module';
 import { TravelHistoryPage } from './travel-history.page';
+import { TravelCardComponent } from './travel-card/travel-card.component';
+import { TravelTicketListComponent } from './travel-ticket-list/travel-ticket-list.component';
+import { TravelTicketDetailComponent } from './travel-ticket-detail/travel-ticket-detail.component';
+import {MaterialModule} from "../../material/material.module";
 
 
 @NgModule({
   declarations: [
-    TravelHistoryPage
+    TravelHistoryPage,
+    TravelCardComponent,
+    TravelTicketListComponent,
+    TravelTicketDetailComponent
   ],
   imports: [
     CommonModule,
-    TravelHistoryRoutingModule
+    TravelHistoryRoutingModule,
+    MaterialModule
   ]
 })
 export class TravelHistoryModule { }
--- src/app/modules/pages/travel-history/travel-history.page.ts
+++ src/app/modules/pages/travel-history/travel-history.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Travel} from "../../api/booking/models/travel";
+import {HeaderService} from "../../shared/ui/header/header.service";
+import {ResService} from "../../api/booking/services/res.service";
+import {User} from "../../api/auth/models/user";
 
 @Component({
   selector: 'app-travel-history',
@@ -7,9 +11,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TravelHistoryPage implements OnInit {
 
-  constructor() { }
+  travelHistory: Travel[] | undefined;
+  filteredTravels: Travel[] | undefined;
+  groupedByBills: Array<Array<Travel>> | undefined;
 
-  ngOnInit(): void {
+  selected: string | undefined;
+
+  constructor(private headerService: HeaderService,
+              private resService: ResService) {
+    headerService.setTitle('Mis Reservas');
   }
 
+  ngOnInit(): void {
+    this.selected = 'Próximas';
+
+    const currentUser: User = JSON.parse(sessionStorage.getItem('currentUser')!);
+
+    this.resService.travels({userId: currentUser.id}).subscribe(
+      (travelHistory => {
+        this.travelHistory = travelHistory;
+        this.filterData('Próximas');
+      }));
+  }
+
+  onSelection(selected: string): void {
+    this.selected = selected;
+    this.filterData(selected);
+  }
+
+  private filterData(filterValue: string): void {
+    if (filterValue === 'Próximas') {
+      this.filteredTravels = this.travelHistory!
+        .filter(travel => travel.state === 'Vendido' && new Date(travel.outputDate!).getTime() > new Date().getTime());
+      this.groupedByBills = this.groupByBill(this.filteredTravels, 'invoiceId');
+    } else if (filterValue === 'Realizadas') {
+      this.filteredTravels = this.travelHistory!
+        .filter(travel => travel.state === 'Confirmado' ||
+          (travel.state === 'Vendido' && new Date(travel.outputDate!).getTime() <= new Date().getTime()));
+      this.groupedByBills = this.groupByBill(this.filteredTravels, 'invoiceId');
+    } else if (filterValue === 'Canceladas') {
+      this.filteredTravels = this.travelHistory!
+        .filter(travel => (travel.state !== 'Vendido' && travel.state !== 'Confirmado')).reverse();
+      this.groupedByBills = this.groupByBill(this.filteredTravels, 'invoiceId');
+    }
+  }
+
+  private groupByBill(xs: Array<any>, key: string): Array<Array<Travel>> {
+    const obj = xs.reduce((rv, x) => {
+      (rv[x[key]] = rv[x[key]] || []).push(x);
+      return rv;
+    }, {});
+
+    return Object.values(obj);
+  }
 }
