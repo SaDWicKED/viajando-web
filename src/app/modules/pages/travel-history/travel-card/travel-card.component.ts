1142dd0d4ee97a34a1ba432e48d103d38b4bfbf8
SaDWicKED <56205681+SaDWicKED@users.noreply.github.com>
2021-08-20 16:36:41 -0400

    Implemented travel-card component

---
+++ src/app/modules/pages/travel-history/travel-card/travel-card.component.ts
@@ -0,0 +1,36 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Travel} from "../../../api/booking/models/travel";
+import {MatDialog} from "@angular/material/dialog";
+import {TravelTicketListComponent} from "../travel-ticket-list/travel-ticket-list.component";
+import {DatePipe} from "@angular/common";
+
+@Component({
+  selector: 'app-travel-card',
+  templateUrl: './travel-card.component.html',
+  styleUrls: ['./travel-card.component.scss']
+})
+export class TravelCardComponent implements OnInit {
+
+  @Input() travel: Travel | undefined;
+  @Input() numberOfSeats: number | undefined;
+  @Input() tickets: Travel[] | undefined;
+
+  totalPrice: number | undefined;
+  state: string | undefined;
+
+  constructor(private dialog: MatDialog,
+              public datePipe: DatePipe) { }
+
+  ngOnInit(): void {
+    this.state = this.travel?.state?.includes('Petición') ? 'peticion' : this.travel?.state;
+    this.totalPrice = this.tickets?.reduce((total, travel) => total + Number(travel.price), 0);
+  }
+
+  onClick() {
+    this.dialog.open(TravelTicketListComponent, {
+      width: '450px',
+      maxHeight: '90vh',
+      data: this.tickets
+    });
+  }
+}
