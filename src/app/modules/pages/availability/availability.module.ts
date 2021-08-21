9e0bc84700ae647475f994377736b1e9ae698de7
SaDWicKED <56205681+SaDWicKED@users.noreply.github.com>
2021-08-20 20:06:35 -0400

    Changed date format on date pickers

--- src/app/modules/pages/availability/availability.module.ts
+++ src/app/modules/pages/availability/availability.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
-import {CommonModule, DatePipe} from '@angular/common';
+import {CommonModule} from '@angular/common';
 
 import { AvailabilityRoutingModule } from './availability-routing.module';
 import { AvailabilityPage } from './availability.page';
 import { AvailabilityPanelComponent } from './availability-panel/availability-panel.component';
 import {MaterialModule} from "../../material/material.module";
 import {ReactiveFormsModule} from "@angular/forms";
+import {MAT_DATE_LOCALE} from "@angular/material/core";
 
 
 @NgModule({
@@ -19,7 +20,7 @@ import {ReactiveFormsModule} from "@angular/forms";
     MaterialModule,
     ReactiveFormsModule
   ],providers: [
-    DatePipe,
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' }
   ]
 })
 export class AvailabilityModule { }
